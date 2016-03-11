ef1715e58b2d1d7796b6d2eb21c2cc8fee70de47
backfrip <backfrip@users.noreply.github.com>
2016-03-11 16:55:05 +0000

    ACK NACK I DON'T EVEN KNOW ANYMORE

--- main.js
+++ main.js
@@ -1,38 +1,46 @@
 var svg = document.getElementById('screen');
+var i, j;
 
 populate();
 
 function populate() {
+  var solution = makeSolution();
   for (var y=0;y<10;y++) {
     for (var x=0;x<10;x++) {
       svg.appendChild(makeTile(50*x, 50*y,
-        'rotate('+(90*(x+y))+','+(50*x+25)+','+(50*y+25)+')'));
+        'rotate('+(90*(x+y))+','+(50*x+25)+','+(50*y+25)+')', Math.floor(Math.random()*2.999999999999)));
     }
   }
 }
 
-function makeTile(x, y, transform) {
+function makeTile(x, y, transform, tileid) {
   var use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
   use.setAttribute('x', x);
   use.setAttribute('y', y);
   use.setAttribute('transform', transform);
-  use.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#tile1');
+  use.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#tile' + tileid);
   use.addEventListener('click', turnTile);
   return use;
 }
 
 function turnTile(e) {
-  var rotate = parse(e.target.getAttribute('transform')).rotate;
+  var rotate = parseTransform(e.target.getAttribute('transform')).rotate;
   e.target.setAttribute('transform',
     'rotate('+(parseInt(rotate[0]) + 90)+','+rotate[1]+','+rotate[2]+')');
 }
 
-function parse(a) {
+function parseTransform(a) {
     var b={};
-    for (var i in a = a.match(/(\w+\((\-?\d+\.?\d*e?\-?\d*,?)+\))+/g))
-    {
+    for (i in a = a.match(/(\w+\((\-?\d+\.?\d*e?\-?\d*,?)+\))+/g)) {
         var c = a[i].match(/[\w\.\-]+/g);
         b[c.shift()] = c;
     }
     return b;
+}
+
+function makeSolution() {
+  var s = [];
+  for (i=0;i<5;i++) {
+    s.push()
+  }
 }
